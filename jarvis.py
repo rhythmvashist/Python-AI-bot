f5381c5cf7cd6891141c1ad10581072407b8144f
Rhythmvashist <vashistrhythm1@gmail.com>
2019-05-19 18:46:46 -0700

    adding various command

--- jarvis.py
+++ jarvis.py
@@ -1,5 +1,8 @@
 import pyttsx3
-from datetime import datetime
+import speech_recognition as sr
+import datetime
+import wikipedia
+import webbrowser
 
 engine =pyttsx3.init('sapi5')
 voices =engine.getProperty('voices')
@@ -11,9 +14,10 @@ def speak(audio):
   engine.say(audio)
   engine.runAndWait()
 
+
 def wishMe():
   hour =int(datetime.datetime.now().hour)
-  if hour >=0 & hour<12:
+  if hour >=0 and hour<12:
     speak("Good Morning")
   elif hour>12 and hour <18:
     speak("good afternoon")
@@ -22,7 +26,57 @@ def wishMe():
 
   speak("Hi I AM JARVIS ,HERE TO HELP")
   
+def takeCommand():
+  r=sr.Recognizer()
+  with sr.Microphone() as source:
+    print("listening ...")
+    r.pause_threshold = 1
+    audio=r.listen(source)
+
+  try:
+    print("Recognising...")
+    query = r.recognize_google(audio,language='en-ca')
+    print(f"user said:{query}\n")
+
+  except Exception as e:
+    print(e)
+    speak("Sorry sir,I couldn't recognise what you said..")
+    print("Say that again Please...")
+    return "None"
+
+  return query;
 
 
 if __name__=="__main__":
-  speak("hello sir")
+  wishMe()
+  while True:
+    quer=takeCommand().lower()
+
+    if 'wikipedia' in quer:
+      speak("searching wikipedia...")
+      quer =quer.replace("wikipedia","")
+      results=wikipedia.summary(quer,sentences=2)
+      speak("according to wkipedia")
+      speak(results)
+    
+    elif 'open youtube' in quer:
+      webbrowser.open("youtube.com")
+    
+    elif 'open open' in quer:
+      webbrowser.open("open.com")
+
+    elif 'open Stackoverflow' in quer:
+      webbrowser.open("stackoverflow.com")
+    
+    elif 'music' in quer:
+      webbrowser.open("spotify.com")
+    
+    elif 'spotify' in quer:
+      webbrowser.open("spotify.com")
+
+    
+
+
+
+
+  #speak("hello sir")
